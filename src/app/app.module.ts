ba18c303c33062b8d8d79734f3a74e0799d5458f
gustavorsan <gustavorsantos42@gmail.com>
2022-12-04 20:48:21 -0300

    Resolução ex3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormEx1Component } from './form-ex1/form-ex1.component';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormEx1Component
+    FormEx1Component,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
