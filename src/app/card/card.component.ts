0419fc12c5a13bbec5e2eac77d89cafbaa10d133
gustavorsan <gustavorsantos42@gmail.com>
2022-12-04 21:06:34 -0300

    resolução ex5

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent {   
+  pesonagem: any = {
+    nome: 'Jon Snow',
+    imagem: 'https://tm.ibxk.com.br/2022/08/26/26174558850404.jpg?    ims=704x264',
+    nomeAtor: 'Kit Harington',
+    descricao:
+      'Ator ator britânico. É mais conhecido por interpretar Jon Snow na     série televisiva Game of Thrones da HBO. Meu marido, mas ele não sabe U.u',
+  };
 
 }
